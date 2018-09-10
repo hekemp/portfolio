c31a2da305e85cf16f6f57d28b38d38915ea4c02
Alic Szecsei <aszecsei@gmail.com>
2018-09-10 18:11:31 -0500

    Add favicon and facebook display metadata.

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,13 @@
-import Document, { Head, Main, NextScript } from "next/document";
-import { ServerStyleSheet } from "styled-components";
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-import "../global-styles";
+import '../global-styles';
 
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet();
     const page = renderPage(App => props =>
-      sheet.collectStyles(<App {...props} />)
+      sheet.collectStyles(<App {...props} />),
     );
     const styleTags = sheet.getStyleElement();
     return { ...page, styleTags };
@@ -20,28 +20,93 @@ export default class MyDocument extends Document {
           <meta charset="utf-8" />
           <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
           <title>Alic Szecsei</title>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
           <meta name="robots" content="index, follow" />
           <meta name="author" content="Alic Szecsei" />
-          <meta name="description" content="Game programming and software development
-          portfolio of Alic Szecsei." />
+          <meta
+            name="description"
+            content="Game programming and software development
+          portfolio of Alic Szecsei."
+          />
+
+          {/* Facebook OpenGraph */}
+          <meta property="og:image:height" content="180" />
+          <meta property="og:image:width" content="344" />
+          <meta property="og:title" content="Alic Szecsei" />
+          <meta
+            property="og:description"
+            content="Game programming and software development portfolio of Alic Szecsei."
+          />
+          <meta property="og:url" content="https://alic-szecsei.com" />
+          <meta
+            property="og:image"
+            content="https://alic-szecsei.com/static/img/og-image.jpg"
+          />
           <meta name="twitter:title" content="Alic Szecsei's Portfolio" />
 
-          {/* third-generation iPad with high-resolution Retina display: */}
-          <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/static/img/favicon144.png" />
-          {/* iPhone with high-resolution retina display: */}
-          <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/static/img/favicon114.png" />
-          {/* first- and second- generation iPad: */}
-          <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/static/img/favicon72.png" />
-          {/* non-retina iPhone, iPod Touch, and Android 2.1+ devices: */}
-          <link rel="apple-touch-icon-precomposed" href="/static/img/favicon57.png" />
-          {/* basic favicon */}
-          <link rel="shortcut icon" href="/static/img/favicon32.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/img/apple-touch-icon.png?v=alQgGE7PNp"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/img/favicon-32x32.png?v=alQgGE7PNp"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/img/favicon-16x16.png?v=alQgGE7PNp"
+          />
+          <link rel="manifest" href="/static/site.webmanifest?v=alQgGE7PNp" />
+          <link
+            rel="mask-icon"
+            href="/static/img/safari-pinned-tab.svg?v=alQgGE7PNp"
+            color="#5bbad5"
+          />
+          <link
+            rel="shortcut icon"
+            href="/static/img/favicon.ico?v=alQgGE7PNp"
+          />
+          <meta name="apple-mobile-web-app-title" content="Alic Szecsei" />
+          <meta name="application-name" content="Alic Szecsei" />
+          <meta name="msapplication-TileColor" content="#603cba" />
+          <meta
+            name="msapplication-config"
+            content="/static/browserconfig.xml?v=alQgGE7PNp"
+          />
+          <meta name="theme-color" content="#ffffff" />
 
-          <link rel="preload" href="/static/fonts/open-sans-v15-latin-italic.woff2" as="font" type="font/woff2" />
-          <link rel="preload" href="/static/fonts/open-sans-v15-latin-regular.woff2" as="font" type="font/woff2" />
-          <link rel="preload" href="/static/fonts/raleway-v12-latin-800.woff2" as="font" type="font/woff2" />
-          <link rel="preload" href="/static/fonts/raleway-v12-latin-regular.woff2" as="font" type="font/woff2" />
+          <link
+            rel="preload"
+            href="/static/fonts/open-sans-v15-latin-italic.woff2"
+            as="font"
+            type="font/woff2"
+          />
+          <link
+            rel="preload"
+            href="/static/fonts/open-sans-v15-latin-regular.woff2"
+            as="font"
+            type="font/woff2"
+          />
+          <link
+            rel="preload"
+            href="/static/fonts/raleway-v12-latin-800.woff2"
+            as="font"
+            type="font/woff2"
+          />
+          <link
+            rel="preload"
+            href="/static/fonts/raleway-v12-latin-regular.woff2"
+            as="font"
+            type="font/woff2"
+          />
           {this.props.styleTags}
         </Head>
         <body>
