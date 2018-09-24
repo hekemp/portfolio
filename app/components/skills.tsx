92a0de3b2dc4987212d33ae7231bc96cabe0f8b7
Alic Szecsei <aszecsei@gmail.com>
2018-09-24 00:25:05 -0500

    Removed skills

--- app/components/skills.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react'
-import styled from 'styled-components'
-
-const SkillWrapper = styled.div`
-  width: 100%;
-  padding: 1em;
-`
--- app/models/skill.ts
+++
@@ -1,8 +0,0 @@
-import skillsList from './skills.json'
-
-export interface ISkill {
-  name: string
-  percent: number
-}
-
-export const skills = skillsList as ISkill[]
